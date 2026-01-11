4af26098d96954ef93754559827a5af5461f6133
Álvaro <amendoma@alumnos.unex.es>
2026-01-11 12:24:16 +0100

    feat: Implemented Aemet service, under future changes

---
+++ app/external_services/aemet_service.py
@@ -0,0 +1,65 @@
+from ..clients.aemet_client import AemetClient, TipoZona, TipoPrediccion
+from ...helpers.aemet_parser import AemetParser
+from app import create_app
+from typing import Optional
+from datetime import date, datetime
+import logging
+
+logger = logging.getLogger(__name__)
+
+class AemetService:
+    app = create_app()
+    cliente = AemetClient(app)
+
+    @staticmethod
+    def get_aemet_data(
+        tipo_zona : TipoZona,
+        tipo_prediccion : TipoPrediccion,
+        codigo_zona : Optional[str],
+        fecha : date
+    ):
+        texto = None
+
+        # Llamada a AemetClient 
+        ## Obtener datos predictivos
+        if tipo_prediccion in [TipoPrediccion.TOMORROW, TipoPrediccion.AFTERTOMORROW]:
+            texto = AemetService.cliente.get_future_data_by_zone(
+                tipo_prediccion = tipo_prediccion,
+                tipo_zone = tipo_zona,
+                ccaa_code = codigo_zona if tipo_zona == TipoZona.CCAA else None,
+                provincia_code = codigo_zona if tipo_zona == TipoZona.PROVINCIAL else None,
+                fecha = fecha
+            ) 
+        else: ## Obtener datos de hoy
+            texto = AemetService.cliente.get_current_data_by_zone(
+                tipo = tipo_zona,
+                ccaa_code = codigo_zona if tipo_zona == TipoZona.CCAA else None,
+                provincia_code = codigo_zona if tipo_zona == TipoZona.PROVINCIAL else None
+            )
+        
+        if texto is None:
+            logger.warning(f"No se ha obtenido datos de AEMET sobre los parámetros indicados - [ {tipo_zona} - {tipo_prediccion} - {codigo_zona} - {fecha}]")
+            return None
+        
+        # Obtenemos datos parseados obtenidos del texto de respuesta por Aemet
+        parseo = AemetParser.parse(texto = texto)
+
+        # Construimos el json para enviar
+        json_final = {
+            "tipo_prediccion" : tipo_prediccion,
+            "tipo_zona" : tipo_zona,
+            "codigo_zona" : codigo_zona,
+            "fecha_prediccion" : fecha.isoformat(),
+            "fecha_elaboracion" : datetime.datetime.now(),
+            "texto_original" : texto,
+            "estado_cielo" : parseo["estado_cielo"],
+            "tendencia_temp_max" : parseo["tendencia_temp_max"],
+            "tendencia_temp_min" : parseo["tendencia_temp_min"],
+            "rachas_viento" : parseo["viento"],
+            "precipitaciones" : parseo["precipitaciones"],
+            "cotas_nieve" : parseo["cota_nieve"],
+            "existencia_helada" : parseo["existencia_helada"],
+            "zona_helada" : parseo["zona_helada"]
+        }
+
+        return json_final
--- helpers/aemet_parser.py
+++ helpers/aemet_parser.py
@@ -4,7 +4,6 @@ from datetime import datetime
 class AemetParser:
 
     def parse(
-        self, 
         texto : str
     ) -> dict:
         """
